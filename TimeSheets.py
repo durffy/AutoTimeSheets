6641f1fc1c62343aeebcc32b0069b230178306fe
cjd <connor_duffy@hotmail.com>
2021-07-21 12:52:18 -0700

    - modifying timesheets to implement ExtraTime and TimeEntry classes

--- TimeSheets.py
+++ TimeSheets.py
@@ -4,6 +4,9 @@ import openpyxl
 from openpyxl import Workbook
 from pathlib import Path
 
+from ExtraTime import ExtraTime
+from TimeEntry import TimeEntry
+
 #for each workbook in a month
     #create a new workbook
     #for each pay period
@@ -15,31 +18,8 @@ from pathlib import Path
 
 
 
-def calcFloatTime(extra_time):
-    ones_decimal_place = str(extra_time).split('.')[1][0]
-
-    if (ones_decimal_place == "1"):
-        return ":10"
-    if (ones_decimal_place == "2"):
-        return ":15"
-    if (ones_decimal_place == "3"):
-        return ":20"
-    if (ones_decimal_place == "4"):
-        return ":25"
-    if (ones_decimal_place == "5"):
-        return ":30"
-    if (ones_decimal_place == "6"):
-        return ":35"
-    if (ones_decimal_place == "7"):
-        return ":40"
-    if (ones_decimal_place == "8"):
-        return ":45"
-    if (ones_decimal_place == "9"):
-        return ":50"
-    else:
-        return ":00"
-
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 for month in months:
 
     files = os.listdir("files/")
@@ -110,33 +90,7 @@ for month in months:
                             destination_ws.append([weekdays[day], am_in, am_out, lunch, pm_in, pm_out, int(total)])
                             destination_ws.append([""])
 
-
-                    if (extra_time >= 8):
-                        pm_out = "4" + calcFloatTime(extra_time)
-                    elif (extra_time >= 7):
-                        pm_out = "3" + calcFloatTime(extra_time)
-                    elif(extra_time >= 6):
-                        pm_out = "2" + calcFloatTime(extra_time)
-                    elif(extra_time >= 5):
-                        pm_out = "1" + calcFloatTime(extra_time)
-                    elif (extra_time >= 4):
-                        pm_in = "12:00"
-                        pm_out = "12" + calcFloatTime(extra_time)
-                    elif (extra_time >= 3):
-                        am_out = "10" + calcFloatTime(extra_time)
-                        lunch = ""
-                        pm_in = ""
-                        pm_out = ""
-                    elif (extra_time >= 2):
-                        am_out = "9" + calcFloatTime(extra_time)
-                        lunch = ""
-                        pm_in = ""
-                        pm_out = ""
-                    elif (extra_time >= 1):
-                        am_out = "8" + calcFloatTime(extra_time)
-                        lunch = ""
-                        pm_in = ""
-                        pm_out = ""
+                    calculatedTime = ExtraTime(extra_time)
 
                     if (extra_time != 0):
                         if (day <5):
@@ -145,7 +99,7 @@ for month in months:
                         if (workday >= 6):
                             print("!!! CHECK HOURS FOR: ")
                         else:
-                            destination_ws.append([weekdays[day], am_in, am_out, lunch, pm_in, pm_out, extra_time])
+                            destination_ws.append([weekdays[day], calculatedTime.time_entry.am_in, calculatedTime.time_entry.am_out, calculatedTime.time_entry.lunch, calculatedTime.time_entry.pm_in, calculatedTime.time_entry.pm_out, extra_time])
                             destination_ws.append([""])
 
 
