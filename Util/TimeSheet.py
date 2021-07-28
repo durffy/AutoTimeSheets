a895da3bf25f5c7a20502116b30ca4792b52749d
cjd <connor_duffy@hotmail.com>
2021-07-28 13:58:39 -0700

    - adding new logic to the timesheet to provide a base hour that then helps calculate the time entries

--- Util/TimeSheet.py
+++ Util/TimeSheet.py
@@ -1,6 +1,7 @@
 """
 This should build a time sheet based on the number of hours provided
 """
+from Util.ExtraTimeEntry import ExtraTime
 from Util.TimeEntry import TimeEntry
 
 
@@ -12,6 +13,7 @@ class TimeSheet():
         self.name = name
         self.date = date
         self.hours = hours
+        self.total_hours = hours
         self.setTimeSheetHeader()
         self.setTimeSheetHours()
         self.setTimeSheetFooter()
@@ -22,36 +24,50 @@ class TimeSheet():
         self.destination_ws.append(["Day", "In", "Out", "Lunch", "In", "Out", "Total"])
 
     def setTimeSheetHours(self):
-        """
-        for the hours in a time sheet
-        if(underFiftySixHours()):
-            iterate by 8 until you cant and iterate the day
-        then iterate by the extra hours left in the day
 
-        """
         weekday = 0
-        extra_time = self.hours % 8
         daily_total = 0
+        if (self.total_hours > 80):
+            print("date: {}, name: {}, hours: {}".format(self.date, self.name, self.hours))
+            base_hour = 13
+        elif (self.total_hours > 72):
+            print("date: {}, name: {}, hours: {}".format(self.date, self.name, self.hours))
+            base_hour = 12
+        elif (self.total_hours  > 64):
+            print("date: {}, name: {}, hours: {}".format(self.date, self.name, self.hours))
+            base_hour = 11
+        elif (self.total_hours  > 56):
+            print("date: {}, name: {}, hours: {}".format(self.date, self.name, self.hours))
+            base_hour = 10
+        elif (self.total_hours  > 48):
+            print("date: {}, name: {}, hours: {}".format(self.date, self.name, self.hours))
+            base_hour = 9
+        else:
+            base_hour = 8
+
+        extra_time = self.hours % base_hour
+
         while self.hours != 0:
 
-            if(self.hours % 8 == 0):
-                print("Divisible by 8")
-                self.hours = self.hours - 8
-                daily_total = 8
-            elif(self.hours % extra_time ==0):
-                print("Divisible by {}".format(extra_time))
+            if(self.hours % base_hour == 0 or self.hours >= base_hour):
+                self.hours = self.hours - base_hour
+                daily_total = base_hour
+
+            elif(self.hours % extra_time == 0):
                 self.hours = self.hours - extra_time
                 daily_total = extra_time
-            else:
-                self.hours = 0
 
-            print(self.hours)
-            default_times = self.setDefaultTimes()
+
+            time_entry = ExtraTime(daily_total)
+            default_times = time_entry.getTimeEntry()
             default_time_list = []
 
-            default_time_list.append(self.__workweek[weekday])
-            if(weekday <= 4):
+
+            if(weekday <= 5):
+                default_time_list.append(self.__workweek[weekday])
                 weekday += 1
+            else:
+                default_time_list.append("Sunday")
 
             for e in default_times.getTimeList():
                 default_time_list.append(e)
@@ -60,11 +76,18 @@ class TimeSheet():
             self.destination_ws.append(default_time_list)
             self.destination_ws.append([""])
 
+        while (weekday <= 4):
+            weekday += 1
+            self.destination_ws.append([self.__workweek[weekday]])
+            self.destination_ws.append([""])
+
 
 
     def setTimeSheetFooter(self):
 
-        self.destination_ws.append(["", "", "", "", "", "Total", self.hours])
+        self.destination_ws.append(["", "", "", "", "", "Total", self.total_hours])
+        self.destination_ws.append([""])
+        self.destination_ws.append([""])
 
     def setDefaultTimes(self):
         defaultTimeEntry = TimeEntry()
